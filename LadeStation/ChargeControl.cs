db23384decdf66fc8b080b0ad6a26e8d9695ee27
PoliteStranger <oliver09@spange.com>
2022-03-10 14:25:34 +0100

    Updated interfaces, tests now work

---
+++ LadeStation/ChargeControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public interface IChargeControl
+    {
+        public bool Connected { get;}
+        public void StartCharge();
+        public void StopCharge();
+
+    }
+    internal class ChargeControl
+    {
+    }
+}
---
+++ LadeStation/Door.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public interface IDoor
+    {
+        public void LockDoor();
+        public void UnlockDoor();
+    }
+    internal class Door
+    {
+    }
+}
