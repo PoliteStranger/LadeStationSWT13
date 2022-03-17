b6686cd160715e360c29ca481c36193c38efbfd5
PoliteStranger <oliver09@spange.com>
2022-03-17 13:22:06 +0100

    changed program.cs

--- LadeStation/Program.cs
+++ LadeStation/Program.cs
@@ -1,8 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ladeskab;
-
-StationControl stationControl = new StationControl();
+﻿
+Console.WriteLine("Hello!!");
