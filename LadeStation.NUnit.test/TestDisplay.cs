9d9e687ac3ba203fda35cfb77487a067495f37ef
Andreas Steen Simmelkiær Sørensen <au196631@uni.au.dk>
2022-03-22 22:30:26 +0100

    Display Nunit tests påbegyndt

---
+++ LadeStation.NUnit.test/TestDisplay.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Ladeskab;
+
+namespace LadeStation.NUnit.test
+{
+    public class TestDisplay
+    {
+
+        private Display _uut;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Display();
+        }
+
+        [Test]
+        public void DoorEventNotNull()
+        {
+            _uut.DisplayGuideMessage(IDisplay.GuideMessages.Free);
+            Assert.That(_uut.GuideStateMessage, Is.EqualTo(IDisplay.GuideMessages.Free));
+        }
+    }
+}
