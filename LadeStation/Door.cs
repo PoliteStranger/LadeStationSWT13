085d707ccc37ae0e906e5b80435e124d26ffb301
patrick8975 <64925851+patrick8975@users.noreply.github.com>
2022-03-22 10:47:07 +0100

    Tjek programmet
    Tjek programmet

--- LadeStation/Door.cs
+++ LadeStation/Door.cs
@@ -6,55 +6,79 @@ using System.Threading.Tasks;
 
 namespace Ladeskab
 {
-   //interface for vores door.
+    public class DTDoorOpenCloseEvent : EventArgs
+    {
+        //Dette er lige som "argumentet man sender med i ens eventhandler"
+        public bool doorOpen { get; set; }
+    }
+
+    //interface for vores door.
     public interface IDoor
     {
-        event EventHandler<DoorOpenCloseEvent> DoorChangedEvent;
-        public void LockDoor();
-        public void UnlockDoor();
+        event EventHandler<DTDoorOpenCloseEvent> DoorChangedEvent;
+        //public void LockDoor();
+        //public void UnlockDoor();
+        public void OnDoorOpen();
+        public void OnDoorClose();
+
+        public void DoorLock();
+        public void DoorUnLock();
     }
 
     public class Door:IDoor
     {
         //eventhandler med vores "argument" DoorOpenCloseEvent
-        public event EventHandler<DoorOpenCloseEvent> DoorChangedEvent;
+        public event EventHandler<DTDoorOpenCloseEvent> DoorChangedEvent;
         //bool for vores lås
-        private bool _lock;
-        public void LockDoor()
+        private bool doorstate;
+        private bool doorlock;
+
+        public Door()
         {
-            //hvis døren er ulåst, så kan den låses. og sende et event
-            if (_lock == false)
-            {
-                DoorStateChanged(new DoorOpenCloseEvent { OpenClose = true });
-                _lock = true;
-            }
-            //ellers er døren fortsat låst.
-            else _lock = true;
+            doorstate = false;
+            doorlock = false;
         }
 
-        public void UnlockDoor()
+        public void OnDoorOpen()
         {
-            //hvis døren er låst, så kan den låses op. og sende et event
-            if (_lock == true)
+            if (doorlock == false)
             {
-                DoorStateChanged(new DoorOpenCloseEvent { OpenClose = false});
-                _lock = false;
-            }
-            //ellers er døren fortsat ulåst.
-            else
-            {
-                _lock = false;
+                DoorStateChanged(new DTDoorOpenCloseEvent { doorOpen = true });
+                doorstate = true;
             }
         }
 
-        protected virtual void DoorStateChanged(DoorOpenCloseEvent e)
+        public void OnDoorClose()
+        {
+            if (doorstate == true)
+            {
+                DoorStateChanged(new DTDoorOpenCloseEvent { doorOpen = false });
+                doorstate = false;
+            }
+        }
+
+        public void DoorLock()
+        {
+            if (doorstate == false)
+            {
+                doorlock = true;
+                Console.WriteLine("Døren er låst");
+            }
+        }
+
+        public void DoorUnLock()
+        {
+            if (doorlock == true)
+            {
+                doorlock = false;
+                Console.WriteLine("Døren er Ulåst");
+            }
+        }
+
+        protected virtual void DoorStateChanged(DTDoorOpenCloseEvent e)
         {
             DoorChangedEvent?.Invoke(this, e);
         }
     }
-    public class DoorOpenCloseEvent : EventArgs
-    {
-        //Dette er lige som "argumentet man sender med i ens eventhandler"
-        public bool OpenClose { get; set; }
-    }
+   
 }
--- LadeStation/Program.cs
+++ LadeStation/Program.cs
@@ -1,2 +1,35 @@
 ﻿
-Console.WriteLine("Hello!!");
+namespace Ladeskab
+{
+    public class test
+    {
+        public bool currentdoorstate { get; set; }
+
+        public test(IDoor door)
+        {
+            door.DoorChangedEvent+=Door_DoorChangedEvent;
+        }
+
+        private void Door_DoorChangedEvent(object sender, DTDoorOpenCloseEvent e)
+        {
+            if (currentdoorstate != e.doorOpen)
+            {
+                currentdoorstate = e.doorOpen;
+                Console.WriteLine("door state is now: {0} \n", currentdoorstate);
+            }
+            else
+            {
+                Console.WriteLine("door state is still: {0} \n", currentdoorstate);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IDoor d1 = new Door();
+            test t1 = new test(d1);
+            d1.OnDoorOpen();
+            d1.DoorLock();
+        }
+    }
+}
+
