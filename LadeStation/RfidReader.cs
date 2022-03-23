111700f3ce4a2f08c403097d621e2e6d79685ff0
PoliteStranger <oliver09@spange.com>
2022-03-23 11:37:15 +0100

    updated functionality

---
+++ LadeStation/RfidReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab
+{
+    public class RfidUpdateArgs : EventArgs
+    {
+        public int Id { get; set; }
+    }
+
+    public interface IRfidReader
+    {
+        event EventHandler<RfidUpdateArgs> RfidUpdateEvent;
+    }
+
+    internal class RfidReader
+    {
+
+    }
+}
